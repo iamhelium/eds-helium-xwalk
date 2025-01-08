c1888c10fe483681052060bb4e3a53d44d0017dc
Tirupa Khokhar <tirupa.khokhar@solarzero.co.nz>
2025-01-09 10:41:12 +1100

    add accordion js change

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,52 +1,69 @@
 export default function decorate(block) {
-    const accordionItems = block.querySelectorAll('.accordion-item');
-    accordionItems.forEach((item) => {
-        const label = item.querySelector('[data-aue-prop="accordionitemlabel"]');
-        const body = item.querySelector('[data-aue-prop="accordionitembody"]');
+  const accordionItems = block.querySelectorAll(
+    '[data-aue-model="accordionitem"]'
+  );
+  accordionItems.forEach((item) => {
+    const labelWrapper = item.querySelector(
+      '[data-aue-prop="accordionitemlabel"]'
+    );
+    const bodyWrapper = item.querySelector(
+      '[data-aue-prop="accordionitembody"]'
+    );
 
-        if (label && body) {
-            const summary = document.createElement('summary');
-            summary.className = 'accordion-item-label';
-            while (label.firstChild) {
-                summary.appendChild(label.firstChild);
-            }
-            label.replaceWith(summary);
-            const bodyWrapper = document.createElement('div');
-            bodyWrapper.className = 'accordion-item-body';
-            while (body.firstChild) {
-                bodyWrapper.appendChild(body.firstChild);
-            }
-            body.replaceWith(bodyWrapper);
-            item.appendChild(summary);
-            item.appendChild(bodyWrapper);
-        }
-    });
-//   [...block.children].forEach((row) => {
-//     const [label, body] = row.children;
+    if (labelWrapper && bodyWrapper) {
+      const summary = document.createElement("summary");
+      summary.className = "accordion-item-label";
 
-//     const summary = document.createElement("summary");
-//     summary.className = "accordion-item-label";
-//     summary.append(...label.childNodes);
-//     label.appendChild(summary);
+      while (labelWrapper.firstChild) {
+        summary.appendChild(labelWrapper.firstChild);
+      }
 
-//     body.classList.add("accordion-item-body");
+      labelWrapper.parentElement.replaceWith(summary);
 
-//     const details = document.createElement("details");
-//     details.className = "accordion-item";
-//     row.parentElement.insertBefore(details, row);
-//     details.appendChild(row);
-//   });
+      const bodyDiv = document.createElement("div");
+      bodyDiv.className = "accordion-item-body";
 
-//   const accordionItems = block.querySelectorAll(".accordion-item");
-//   accordionItems.forEach((item) => {
-//     item.addEventListener("toggle", () => {
-//       if (item.open) {
-//         accordionItems.forEach((otherItem) => {
-//           if (otherItem !== item) {
-//             otherItem.open = false;
-//           }
-//         });
-//       }
-//     });
-//   });
+      while (bodyWrapper.firstChild) {
+        bodyDiv.appendChild(bodyWrapper.firstChild);
+      }
+
+      bodyWrapper.parentElement.replaceWith(bodyDiv);
+
+      const details = document.createElement("details");
+      details.className = "accordion-item";
+
+      details.appendChild(summary);
+      details.appendChild(bodyDiv);
+
+      item.replaceWith(details);
+    }
+  });
+  //   [...block.children].forEach((row) => {
+  //     const [label, body] = row.children;
+
+  //     const summary = document.createElement("summary");
+  //     summary.className = "accordion-item-label";
+  //     summary.append(...label.childNodes);
+  //     label.appendChild(summary);
+
+  //     body.classList.add("accordion-item-body");
+
+  //     const details = document.createElement("details");
+  //     details.className = "accordion-item";
+  //     row.parentElement.insertBefore(details, row);
+  //     details.appendChild(row);
+  //   });
+
+  //   const accordionItems = block.querySelectorAll(".accordion-item");
+  //   accordionItems.forEach((item) => {
+  //     item.addEventListener("toggle", () => {
+  //       if (item.open) {
+  //         accordionItems.forEach((otherItem) => {
+  //           if (otherItem !== item) {
+  //             otherItem.open = false;
+  //           }
+  //         });
+  //       }
+  //     });
+  //   });
 }
