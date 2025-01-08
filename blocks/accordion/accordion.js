1932aae6068e6b09366880dd1f41f02a59478822
Tirupa Khokhar <tirupa.khokhar@solarzero.co.nz>
2025-01-09 10:27:13 +1100

    add accordion js change

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,30 +1,30 @@
 export default function decorate(block) {
-    [...block.children].forEach((row) => {
-      // decorate accordion item label
-      const label = row.children[0];
-      const summary = document.createElement('summary');
-      summary.className = 'accordion-item-label';
-      summary.append(...label.childNodes);
-      // decorate accordion item body
-      const body = row.children[1];
-      body.className = 'accordion-item-body';
-      // decorate accordion item
-      const details = document.createElement('details');
-      details.className = 'accordion-item';
-      details.append(summary, body);
-      row.replaceWith(details);
-    });
+  [...block.children].forEach((row) => {
+    const [label, body] = row.children;
 
-    const accordionItems = block.querySelectorAll('.accordion-item');
-    accordionItems.forEach((item) => {
-        item.addEventListener('toggle', () => {
-            if (item.open) {
-                accordionItems.forEach((otherItem) => {
-                if (otherItem !== item) {
-                    otherItem.open = false;
-                }
-                });
-            }
+    const summary = document.createElement("summary");
+    summary.className = "accordion-item-label";
+    summary.append(...label.childNodes);
+    label.appendChild(summary);
+
+    body.classList.add("accordion-item-body");
+
+    const details = document.createElement("details");
+    details.className = "accordion-item";
+    row.parentElement.insertBefore(details, row);
+    details.appendChild(row);
+  });
+
+  const accordionItems = block.querySelectorAll(".accordion-item");
+  accordionItems.forEach((item) => {
+    item.addEventListener("toggle", () => {
+      if (item.open) {
+        accordionItems.forEach((otherItem) => {
+          if (otherItem !== item) {
+            otherItem.open = false;
+          }
         });
+      }
     });
-  }
+  });
+}
