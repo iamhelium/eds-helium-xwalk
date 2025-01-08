4447c69147da8bd4d397a69da025e0ad0892694e
Tirupa Khokhar <tirupa.khokhar@solarzero.co.nz>
2025-01-09 10:14:53 +1100

    add accordion js change

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,33 +1,30 @@
 export default function decorate(block) {
-        [...block.children].forEach((row) => {
-        const label = row.children[0];
-        const body = row.children[1];
-    
-        const summary = document.createElement('summary');
-        summary.className = 'accordion-item-label';
-        summary.append(...label.childNodes);
-        label.appendChild(summary);
-    
-        body.classList.add('accordion-item-body');
-    
-        const details = document.createElement('details');
-        details.className = 'accordion-item';
-        details.append(label, body);
-    
-        row.replaceWith(details);
+    [...block.children].forEach((row) => {
+      // decorate accordion item label
+      const label = row.children[0];
+      const summary = document.createElement('summary');
+      summary.className = 'accordion-item-label';
+      summary.append(...label.childNodes);
+      // decorate accordion item body
+      const body = row.children[1];
+      body.className = 'accordion-item-body';
+      // decorate accordion item
+      const details = document.createElement('details');
+      details.className = 'accordion-item';
+      details.append(summary, body);
+      row.replaceWith(details);
     });
-  
+
     const accordionItems = block.querySelectorAll('.accordion-item');
     accordionItems.forEach((item) => {
         item.addEventListener('toggle', () => {
             if (item.open) {
                 accordionItems.forEach((otherItem) => {
-                    if (otherItem !== item) {
+                if (otherItem !== item) {
                     otherItem.open = false;
-                    }
+                }
                 });
             }
         });
     });
   }
-  
