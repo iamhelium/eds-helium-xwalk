a95c6e1946c7b46c674ca5842f9130f8f80c6797
Tirupa Khokhar <tirupa.khokhar@solarzero.co.nz>
2025-01-09 11:51:51 +1100

    add accordion css js change

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,15 +1,16 @@
 export default function decorate(block) {
-document.querySelectorAll('[data-aue-model="accordionitem"]').forEach((item) => {
-    const label = item.querySelector('[data-aue-prop="accordionitemlabel"]');
-    const body = item.querySelector('[data-aue-prop="accordionitembody"]');
-
-    label.addEventListener('click', () => {
-        document.querySelectorAll('[data-aue-model="accordionitem"].open').forEach((openItem) => {
-        if (openItem !== item) {
-            openItem.classList.remove('open');
-        }
+    document.querySelectorAll('[data-aue-model="accordionitem"]').forEach((item) => {
+        const label = item.querySelector('[data-aue-prop="accordionitemlabel"]');
+        const body = item.querySelector('[data-aue-prop="accordionitembody"]');
+        label.addEventListener('click', () => {
+            item.classList.toggle('open');
+            const accordionWrapper = item.closest('.accordion-wrapper');
+            const otherItems = accordionWrapper.querySelectorAll('[data-aue-model="accordionitem"].open');
+            otherItems.forEach((openItem) => {
+                if (openItem !== item) {
+                    openItem.classList.remove('open');
+                }
+            });
         });
-        item.classList.toggle('open');
     });
-});
 }
