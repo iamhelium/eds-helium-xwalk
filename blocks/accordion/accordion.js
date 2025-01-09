d96168f23ff2d319ab6c2890a4f91980c7e9fa80
Tirupa Khokhar <tirupa.khokhar@solarzero.co.nz>
2025-01-09 12:01:53 +1100

    add accordion item toggle js change

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -2,14 +2,17 @@ export default function decorate(block) {
     document.querySelectorAll('.accordion-wrapper').forEach((accordionWrapper) => {
         accordionWrapper.querySelectorAll('[data-aue-model="accordionitem"]').forEach((item) => {
             const label = item.querySelector('[data-aue-prop="accordionitemlabel"]');
-            const body = item.querySelector('[data-aue-prop="accordionitembody"]');
             label.addEventListener('click', () => {
                 accordionWrapper.querySelectorAll('[data-aue-model="accordionitem"].open').forEach((openItem) => {
                     if (openItem !== item) {
                         openItem.classList.remove('open');
                     }
                 });
-                item.classList.toggle('open');
+                if (item.classList.contains('open')) {
+                    item.classList.remove('open');
+                } else {
+                    item.classList.add('open');
+                }
             });
         });
     });
