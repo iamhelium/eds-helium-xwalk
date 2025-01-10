52ded1167bc3451021828e3cef90d5d7b2cfa996
Tirupa Khokhar <tirupa.khokhar@solarzero.co.nz>
2025-01-10 11:53:38 +1100

    commect decorateButton function

--- scripts/aem.js
+++ scripts/aem.js
@@ -405,45 +405,45 @@ function wrapTextNodes(block) {
  * Decorates paragraphs containing a single link as buttons.
  * @param {Element} element container element
  */
-// function decorateButtons(element) {
-//   element.querySelectorAll('a').forEach((a) => {
-//     a.title = a.title || a.textContent;
-//     if (a.href !== a.textContent) {
-//       const up = a.parentElement;
-//       const twoup = a.parentElement.parentElement;
-//       if (!a.querySelector('img')) {
-//         if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-//           a.className = 'button'; // default
-//           up.classList.add('button-container');
-//         }
-//         if (
-//           up.childNodes.length === 1
-//           && up.tagName === 'STRONG'
-//           && twoup.childNodes.length === 1
-//           && twoup.tagName === 'P'
-//         ) {
-//           a.className = 'button primary';
-//           twoup.classList.add('button-container');
-//         }
-//         if (
-//           up.childNodes.length === 1
-//           && up.tagName === 'EM'
-//           && twoup.childNodes.length === 1
-//           && twoup.tagName === 'P'
-//         ) {
-//           a.className = 'button new';
-//           twoup.classList.add('button-container');
-//         }console.log('length == ',up.childNodes, twoup.childNodes);
-//         if (
-//           up.childNodes.length === 2
-//         ) {
-//           a.className = 'button new';
-//           twoup.classList.add('button-container');
-//         }
-//       }
-//     }
-//   });
-// }
+function decorateButtons(element) {
+  // element.querySelectorAll('a').forEach((a) => {
+  //   a.title = a.title || a.textContent;
+  //   if (a.href !== a.textContent) {
+  //     const up = a.parentElement;
+  //     const twoup = a.parentElement.parentElement;
+  //     if (!a.querySelector('img')) {
+  //       if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
+  //         a.className = 'button'; // default
+  //         up.classList.add('button-container');
+  //       }
+  //       if (
+  //         up.childNodes.length === 1
+  //         && up.tagName === 'STRONG'
+  //         && twoup.childNodes.length === 1
+  //         && twoup.tagName === 'P'
+  //       ) {
+  //         a.className = 'button primary';
+  //         twoup.classList.add('button-container');
+  //       }
+  //       if (
+  //         up.childNodes.length === 1
+  //         && up.tagName === 'EM'
+  //         && twoup.childNodes.length === 1
+  //         && twoup.tagName === 'P'
+  //       ) {
+  //         a.className = 'button new';
+  //         twoup.classList.add('button-container');
+  //       }console.log('length == ',up.childNodes, twoup.childNodes);
+  //       if (
+  //         up.childNodes.length === 2
+  //       ) {
+  //         a.className = 'button new';
+  //         twoup.classList.add('button-container');
+  //       }
+  //     }
+  //   }
+  // });
+}
 
 /**
  * Add <img> for icon, prefixed with codeBasePath and optional prefix.
