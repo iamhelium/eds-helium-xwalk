a98e465533dcf61830ada7e58ea0226d672e3bbb
Tirupa Khokhar <tirupa.khokhar@solarzero.co.nz>
2025-01-15 11:49:45 +1100

    add button js

---
+++ blocks/heliumbutton/heliumbutton.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+    const textElement = block.querySelector('[data-aue-prop="text"]');
+    const linkElement = block.querySelector('a.button');
+  
+    if (textElement && linkElement) {
+      const buttonText = textElement.textContent.trim();
+      const linkHref = linkElement.getAttribute('href');
+
+      const button = document.createElement('button');
+      button.textContent = buttonText;
+      button.className = '';
+  
+      button.addEventListener('click', () => {
+        window.location.href = linkHref;
+      });
+  
+      block.innerHTML = '';
+      block.appendChild(button);
+    }
+  }
+  
