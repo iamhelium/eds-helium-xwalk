bb8c588f04b50f50be8ffa5d9a369b410a0a8ff5
Tirupa Khokhar <tirupa.khokhar@solarzero.co.nz>
2025-01-21 10:13:46 +1100

    add aem tag in heliumbutton model

--- scripts/test-graphql-local.js
+++ scripts/test-graphql-local.js
@@ -1,33 +1,4 @@
-// export async function graphqlDemoGet() {
-//     const baseApiUrl = 'https://author-p51328-e442308.adobeaemcloud.com/graphql/execute.json';
-//     const projectId = 'my-project';
-//     const queryName = 'all-teams';
-//     const queryParams = ''; // Adjust if needed
-//     const graphqlEndpoint = `${baseApiUrl}/${projectId}/${queryName}${queryParams}`;
-//     const username = 'admin';
-//     const password = 'admin';
-//     const base64Credentials = btoa(username + ':' + password);
-  
-//     // Return the fetch promise chain so that it can be awaited outside
-//     return fetch(graphqlEndpoint, {
-//         method: 'GET',
-//         headers: {
-//             'Authorization': `Basic ${base64Credentials}`,
-//         },
-//     }).then(response => {
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//         return response.json();
-//     }).then(data => {
-//         return data; // Make sure to return the data so that the promise resolves with it
-//     }).catch(error => {
-//         console.error('Error fetching data: ', error);
-//         throw error; // Rethrow or handle error as appropriate
-//     });
-//   }
-
-  export async function graphqlDemoGet() {
+export async function graphqlDemoGet() {
     const baseApiUrl = '/graphql/execute.json';
     const projectId = 'aem-demo-assets';
     const queryName = 'articles-all';
@@ -48,4 +19,4 @@
         console.error('Error fetching data: ', error);
         throw error; // Rethrow or handle the error as appropriate
     });
-  }
+}
