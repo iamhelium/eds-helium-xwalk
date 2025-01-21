657c02145553bb5c0b5d535b143f6e7924261f14
Tirupa Khokhar <tirupa.khokhar@solarzero.co.nz>
2025-01-21 12:45:40 +1100

    add change to heliumbutton js

--- blocks/heliumbutton/heliumbutton.js
+++ blocks/heliumbutton/heliumbutton.js
@@ -1,21 +1,49 @@
-export default function decorate(block) {
-    const textElement = block.querySelector('[data-aue-prop="text"]');
-    const linkElement = block.querySelector('a.button');
-  
-    if (textElement && linkElement) {
-      const buttonText = textElement.textContent.trim();
-      const linkHref = linkElement.getAttribute('href');
+import { openModal } from '../modal/modal';
 
-      const button = document.createElement('button');
-      button.textContent = buttonText;
-      button.className = '';
-  
-      button.addEventListener('click', () => {
-        window.location.href = linkHref;
-      });
-  
-      block.innerHTML = '';
-      block.appendChild(button);
-    }
+export default function decorate(block) {
+  const textElement = block.querySelector('[data-aue-prop="text"]');
+  const linkElement = block.querySelector('a.button');
+
+  if (textElement && linkElement) {
+    const buttonText = textElement.textContent.trim();
+    const linkHref = linkElement.getAttribute('href');
+
+    const button = document.createElement('button');
+    button.textContent = buttonText;
+    button.className = '';
+
+    button.addEventListener('click', async () => {
+      if (linkHref) {
+        await openModal(linkHref);
+      } else {
+        console.error('No link provided for modal content.');
+      }
+    });
+
+    block.innerHTML = '';
+    block.appendChild(button);
   }
+}
+
+
+// export default function decorate(block) {
+//     const textElement = block.querySelector('[data-aue-prop="text"]');
+//     const linkElement = block.querySelector('a.button');
+  
+//     if (textElement && linkElement) {
+//       const buttonText = textElement.textContent.trim();
+//       const linkHref = linkElement.getAttribute('href');
+
+//       const button = document.createElement('button');
+//       button.textContent = buttonText;
+//       button.className = '';
+  
+//       button.addEventListener('click', () => {
+//         window.location.href = linkHref;
+//       });
+  
+//       block.innerHTML = '';
+//       block.appendChild(button);
+//     }
+//   }
   
