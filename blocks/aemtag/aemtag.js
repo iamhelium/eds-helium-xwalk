e95ec13ba97518be97bcbf46767b9950cec37e27
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-03-13 22:04:41 +0530

    console tag

--- blocks/aemtag/aemtag.js
+++ blocks/aemtag/aemtag.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
   const metaTag = document.querySelector('meta[name="cq-tags"]');
   const content = metaTag ? metaTag.getAttribute('content') : null;
-  console.log(content);
+  console.log('AEM TAG: ', content);
 }
