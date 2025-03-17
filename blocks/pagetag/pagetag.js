c540b351b2be5a151584651bb7cf232b978b412b
JananiVM <jananivmtce@gmail.com>
2025-03-18 10:54:36 +1100

    Added pagetag block

---
+++ blocks/pagetag/pagetag.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+export default async function decorate(block) {
+   const metaTag = document.querySelector('meta[name="cq-tags"]');
+    const content = metaTag ? metaTag.getAttribute('content') : null;
+    console.log('AEM TAG: ', content);
+}
