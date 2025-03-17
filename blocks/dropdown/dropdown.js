8c10c49d86ddb29c419fd765f7fc2d37169aae6d
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-03-17 23:33:04 +0530

    dropdown v1

--- blocks/dropdown/dropdown.js
+++ blocks/dropdown/dropdown.js
@@ -1,3 +1,22 @@
-export default function decorate(block) {
-  console.log(block);
+/* eslint-disable linebreak-style */
+export default async function decorate(block) {
+  try {
+    const response = await fetch('/graphql/execute.json/eds-helium-xwalk/Sample');
+    const data = await response.json();
+
+    const items = data?.data?.edsXwalkModelList?.items || [];
+
+    const dropdown = document.createElement('select');
+    dropdown.classList.add('custom-dropdown');
+
+    items.forEach((item) => {
+      const option = document.createElement('option');
+      option.textContent = item.name ?? '';
+      dropdown.appendChild(option);
+    });
+
+    block.appendChild(dropdown);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
