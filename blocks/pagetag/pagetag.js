f8bcecd2d55d31c03196a6ba0587b40638284fd2
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-03-19 00:06:00 +0530

    check now page tag

--- blocks/pagetag/pagetag.js
+++ blocks/pagetag/pagetag.js
@@ -1,7 +1,6 @@
 /* eslint-disable linebreak-style */
 export default async function decorate(block) {
-//   const metaTag = document.querySelector('meta[name="cq-tags"]');
-//   const content = metaTag ? metaTag.getAttribute('content') : null;
-//   console.log('AEM TAG: ', content, block);
-  console.log('AEM TAG: ', block);
+  const metaTag = document.querySelector('meta[name="cq-tags"]');
+  const content = metaTag ? metaTag.getAttribute('content') : null;
+  console.log('AEM TAG: ', content, block);
 }
