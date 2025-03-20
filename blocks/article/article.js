95811a2ea232d44c9713f65b4c2c7a1f6fdf5e1e
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-03-21 00:25:48 +0530

    udpate dynamic

--- blocks/article/article.js
+++ blocks/article/article.js
@@ -61,7 +61,7 @@ function filterArticles(block, finalJson, selectedTag) {
 if(articleWrapper){
   articleWrapper.appendChild(articleCardWrapper);
 } else {
-  block.innerHTML = articleCardWrapper;
+  articleWrapper.replaceWith(articleCardWrapper);
 }
 }
 
