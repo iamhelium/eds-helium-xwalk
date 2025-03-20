811d53d5deb18d53bb85774b406fdfc2614f616e
JananiVM <jananivmtce@gmail.com>
2025-03-20 11:42:03 +1100

    Updated tags sidekick

--- tools/sidekick/plugins/tags/tags.js
+++ tools/sidekick/plugins/tags/tags.js
@@ -1,16 +1,5 @@
-/*
- * Copyright 2023 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
-
-import { PLUGIN_EVENTS } from 'https://main--franklin-library-host--dylandepass.hlx.live/tools/sidekick/library/events/events.js';
+// eslint-disable-next-line import/no-unresolved
+import { PLUGIN_EVENTS } from 'https://www.aem.live/tools/sidekick/library/events/events.js';
 
 const selectedTags = [];
 
@@ -23,20 +12,26 @@ function getFilteredTags(data, query) {
     return data;
   }
 
-  return data.filter(item => item.tag.toLowerCase().includes(query.toLowerCase()));
+  return data.filter((item) => item.tag.toLowerCase().includes(query.toLowerCase()));
 }
 
 export async function decorate(container, data, query) {
+  if (!data) {
+    // eslint-disable-next-line no-console
+    console.warn('Tag sheet is not configured');
+    return;
+  }
   const createMenuItems = () => {
     const filteredTags = getFilteredTags(data, query);
     return filteredTags.map((item) => {
       const isSelected = selectedTags.includes(item.tag);
       return `
-        <sp-menu-item value="${item.tag}" ${isSelected ? "selected" : ""}>
-          ${item.tag}
-        </sp-menu-item>
-      `;
-    }).join("");
+      <div class="tag-item-wrapper">
+        <img class="icon-img tag" src="/tools/sidekick/plugins/tags-advance/icons/tag.png">
+        <img class="icon-img tag-fill" src="/tools/sidekick/plugins/tags-advance/icons/tag-filled.png">
+        <sp-menu-item value="${item.tag}" ${isSelected ? 'selected' : ''}>${item.tag}</sp-menu-item>
+      </div>`;
+    }).join('');
   };
 
   const handleMenuItemClick = (e) => {
@@ -50,16 +45,16 @@ export async function decorate(container, data, query) {
       selectedTags.push(value);
     }
 
-    const selectedLabel = container.querySelector(".selectedLabel");
+    const selectedLabel = container.querySelector('.selectedLabel');
     selectedLabel.textContent = getSelectedLabel();
   };
 
   const handleCopyButtonClick = () => {
-    navigator.clipboard.writeText(selectedTags.join(", "));
+    navigator.clipboard.writeText(selectedTags.join(', '));
     container.dispatchEvent(
       new CustomEvent(PLUGIN_EVENTS.TOAST, {
-        detail: { message: "Copied Tags" },
-      })
+        detail: { message: 'Copied Tags' },
+      }),
     );
   };
 
@@ -68,12 +63,12 @@ export async function decorate(container, data, query) {
     <sp-menu
       label="Select tags"
       selects="multiple"
-      data-testid="taxonomy"
     >
       ${menuItems}
     </sp-menu>
     <sp-divider size="s"></sp-divider>
     <div class="footer">
+    <sp-icon-info slot="icon"></sp-icon-info>
       <span class="selectedLabel">${getSelectedLabel()}</span>
       <sp-action-button label="Copy" quiet>
         <sp-icon-copy slot="icon"></sp-icon-copy>
@@ -81,18 +76,18 @@ export async function decorate(container, data, query) {
     </div>
   `;
 
-  const spContainer = document.createElement("div");
+  const spContainer = document.createElement('div');
   spContainer.classList.add('container');
   spContainer.innerHTML = sp;
   container.append(spContainer);
 
-  const menuItemElements = spContainer.querySelectorAll("sp-menu-item");
+  const menuItemElements = spContainer.querySelectorAll('sp-menu-item');
   menuItemElements.forEach((item) => {
-    item.addEventListener("click", handleMenuItemClick);
+    item.addEventListener('click', handleMenuItemClick);
   });
 
-  const copyButton = spContainer.querySelector("sp-action-button");
-  copyButton.addEventListener("click", handleCopyButtonClick);
+  const copyButton = spContainer.querySelector('sp-action-button');
+  copyButton.addEventListener('click', handleCopyButtonClick);
 }
 
 export default {
