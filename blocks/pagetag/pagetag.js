5502e7e67472c4857264d123b05829044a86722b
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-03-23 23:20:54 +0530

    remove pagetag

--- blocks/pagetag/pagetag.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable linebreak-style */
-export default async function decorate(block) {
-  const metaTag = document.querySelector('meta[name="cq-tags"]');
-  const content = metaTag ? metaTag.getAttribute('content') : null;
-  console.log('FROM {Pagetag.js} AEM TAG: ', content, block);
-}
