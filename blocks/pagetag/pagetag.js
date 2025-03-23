8cb65a1a81ec039191ff3763783a1c6dc1301d1f
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-03-23 23:15:03 +0530

    udpate pagetag

--- blocks/pagetag/pagetag.js
+++ blocks/pagetag/pagetag.js
@@ -2,5 +2,5 @@
 export default async function decorate(block) {
   const metaTag = document.querySelector('meta[name="cq-tags"]');
   const content = metaTag ? metaTag.getAttribute('content') : null;
-  console.log('AEM TAG: ', content, block);
+  console.log('FROM {Pagetag.js} AEM TAG: ', content, block);
 }
