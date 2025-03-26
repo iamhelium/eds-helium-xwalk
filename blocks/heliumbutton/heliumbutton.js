bbda495cda41b3b366e0268cc22e7fa1a49f0297
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-03-26 23:24:34 +0530

    fix helium button

--- blocks/heliumbutton/heliumbutton.js
+++ blocks/heliumbutton/heliumbutton.js
@@ -1,6 +1,23 @@
 import { openModal } from '../modal/modal.js';
 import { graphqlDemoGet } from '../../scripts/test-graphql-local.js';
 
+function renderData(data, targetElement) {
+  const dataContainer = document.createElement('div');
+  dataContainer.className = 'graphql-data';
+
+  if (data?.data?.articlePaginated?.edges) {
+    data.data.articlePaginated?.edges.forEach((edge) => {
+      const edgeElement = document.createElement('p');
+      edgeElement.textContent = `${edge.node.authorFragment.firstName} ${edge.node.authorFragment.lastName}`;
+      dataContainer.appendChild(edgeElement);
+    });
+  } else {
+    dataContainer.textContent = 'No data available.';
+  }
+
+  targetElement.appendChild(dataContainer);
+}
+
 export default function decorate(block) {
   const textElement = block.querySelector('[data-aue-prop="text"]');
   const linkElement = block.querySelector('a.button');
@@ -33,28 +50,10 @@ export default function decorate(block) {
   }
 }
 
-function renderData(data, targetElement) {
-  const dataContainer = document.createElement('div');
-  dataContainer.className = 'graphql-data';
-
-  if (data?.data?.articlePaginated?.edges) {
-    data.data.articlePaginated?.edges.forEach((edge) => {
-      const edgeElement = document.createElement('p');
-      edgeElement.textContent = edge.node.authorFragment.firstName+ " " + edge.node.authorFragment.lastName;
-      dataContainer.appendChild(edgeElement);
-    });
-  } else {
-    dataContainer.textContent = 'No data available.';
-  }
-
-  targetElement.appendChild(dataContainer);
-}
-
-
 // export default function decorate(block) {
 //     const textElement = block.querySelector('[data-aue-prop="text"]');
 //     const linkElement = block.querySelector('a.button');
-  
+
 //     if (textElement && linkElement) {
 //       const buttonText = textElement.textContent.trim();
 //       const linkHref = linkElement.getAttribute('href');
@@ -62,13 +61,12 @@ function renderData(data, targetElement) {
 //       const button = document.createElement('button');
 //       button.textContent = buttonText;
 //       button.className = '';
-  
+
 //       button.addEventListener('click', () => {
 //         window.location.href = linkHref;
 //       });
-  
+
 //       block.innerHTML = '';
 //       block.appendChild(button);
 //     }
 //   }
-  
